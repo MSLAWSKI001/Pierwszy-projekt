a8eb6aca5e9253b030aa0da3ad72e35b4215e89a
MSLAWSKI001 <113010458+MSLAWSKI001@users.noreply.github.com>
2023-02-27 14:22:59 +0100

    Add files via upload

---
+++ TestApp/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int testResults()
+        {
+            int wynik = 0;
+            
+
+            if (Button4.Checked)
+            {
+                wynik++;
+            }
+            if (Buttonłyba.Checked)
+            {
+                wynik++;
+            }
+            if (checkBox1.Checked && checkBox2.Checked && checkBox3.Checked)
+            {
+                wynik++;
+                if (checkBox1.Checked && checkBox2.Checked && checkBox3.Checked && checkBox4.Checked)
+                {
+                    wynik--;
+                }
+                
+            }
+            if (textBox1.Text == "Ziemia" || textBox1.Text == "ziemia")
+            {
+                wynik++;
+            }
+
+
+
+            return wynik;
+        }
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+
+
+            MessageBox.Show("Ilosc prawidłowych: " + testResults());
+        }
+
+        
+    }
+}
