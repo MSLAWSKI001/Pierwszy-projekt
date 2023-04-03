ac7353f20e60f68cca08ebfeafd79d424c9ffbd5
MSLAWSKI001 <113010458+MSLAWSKI001@users.noreply.github.com>
2023-04-03 13:30:00 +0200

    Add files via upload

---
+++ TestApp/MainForm.Designer.cs
@@ -0,0 +1,348 @@
+﻿
+namespace TestApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Button4 = new System.Windows.Forms.RadioButton();
+            this.Button3 = new System.Windows.Forms.RadioButton();
+            this.Button2 = new System.Windows.Forms.RadioButton();
+            this.Button1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Buttonłyba = new System.Windows.Forms.RadioButton();
+            this.Buttonkot = new System.Windows.Forms.RadioButton();
+            this.Buttonpapuga = new System.Windows.Forms.RadioButton();
+            this.Buttonpies = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Button4);
+            this.groupBox1.Controls.Add(this.Button3);
+            this.groupBox1.Controls.Add(this.Button2);
+            this.groupBox1.Controls.Add(this.Button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(26, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(110, 163);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pytanie 1";
+            // 
+            // Button4
+            // 
+            this.Button4.AutoSize = true;
+            this.Button4.Location = new System.Drawing.Point(0, 124);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(31, 19);
+            this.Button4.TabIndex = 4;
+            this.Button4.TabStop = true;
+            this.Button4.Text = "4";
+            this.Button4.UseVisualStyleBackColor = true;
+            // 
+            // Button3
+            // 
+            this.Button3.AutoSize = true;
+            this.Button3.Location = new System.Drawing.Point(0, 99);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(31, 19);
+            this.Button3.TabIndex = 3;
+            this.Button3.TabStop = true;
+            this.Button3.Text = "3";
+            this.Button3.UseVisualStyleBackColor = true;
+            // 
+            // Button2
+            // 
+            this.Button2.AutoSize = true;
+            this.Button2.Location = new System.Drawing.Point(0, 74);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(31, 19);
+            this.Button2.TabIndex = 2;
+            this.Button2.TabStop = true;
+            this.Button2.Text = "2";
+            this.Button2.UseVisualStyleBackColor = true;
+            // 
+            // Button1
+            // 
+            this.Button1.AutoSize = true;
+            this.Button1.Location = new System.Drawing.Point(0, 49);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(31, 19);
+            this.Button1.TabIndex = 1;
+            this.Button1.TabStop = true;
+            this.Button1.Text = "1";
+            this.Button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(0, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ile to 2+2";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Buttonłyba);
+            this.groupBox2.Controls.Add(this.Buttonkot);
+            this.groupBox2.Controls.Add(this.Buttonpapuga);
+            this.groupBox2.Controls.Add(this.Buttonpies);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(170, 33);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(126, 163);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pytanie 2";
+            // 
+            // Buttonłyba
+            // 
+            this.Buttonłyba.AutoSize = true;
+            this.Buttonłyba.Location = new System.Drawing.Point(0, 124);
+            this.Buttonłyba.Name = "Buttonłyba";
+            this.Buttonłyba.Size = new System.Drawing.Size(47, 19);
+            this.Buttonłyba.TabIndex = 4;
+            this.Buttonłyba.TabStop = true;
+            this.Buttonłyba.Text = "łybę";
+            this.Buttonłyba.UseVisualStyleBackColor = true;
+            // 
+            // Buttonkot
+            // 
+            this.Buttonkot.AutoSize = true;
+            this.Buttonkot.Location = new System.Drawing.Point(0, 99);
+            this.Buttonkot.Name = "Buttonkot";
+            this.Buttonkot.Size = new System.Drawing.Size(48, 19);
+            this.Buttonkot.TabIndex = 3;
+            this.Buttonkot.TabStop = true;
+            this.Buttonkot.Text = "kota";
+            this.Buttonkot.UseVisualStyleBackColor = true;
+            // 
+            // Buttonpapuga
+            // 
+            this.Buttonpapuga.AutoSize = true;
+            this.Buttonpapuga.Location = new System.Drawing.Point(0, 74);
+            this.Buttonpapuga.Name = "Buttonpapuga";
+            this.Buttonpapuga.Size = new System.Drawing.Size(65, 19);
+            this.Buttonpapuga.TabIndex = 2;
+            this.Buttonpapuga.TabStop = true;
+            this.Buttonpapuga.Text = "papugę";
+            this.Buttonpapuga.UseVisualStyleBackColor = true;
+            // 
+            // Buttonpies
+            // 
+            this.Buttonpies.AutoSize = true;
+            this.Buttonpies.Location = new System.Drawing.Point(0, 49);
+            this.Buttonpies.Name = "Buttonpies";
+            this.Buttonpies.Size = new System.Drawing.Size(43, 19);
+            this.Buttonpies.TabIndex = 1;
+            this.Buttonpies.TabStop = true;
+            this.Buttonpies.Text = "psa";
+            this.Buttonpies.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(0, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "co miała Ala?";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkBox4);
+            this.groupBox3.Controls.Add(this.checkBox3);
+            this.groupBox3.Controls.Add(this.checkBox2);
+            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(329, 33);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(205, 163);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Pytanie 3";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(7, 125);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(38, 19);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "65";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(7, 100);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(38, 19);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "66";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 75);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(38, 19);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "26";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 48);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(32, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "2";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(0, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "zaznacz parzyste liczby";
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(424, 280);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(112, 57);
+            this.buttonCheck.TabIndex = 4;
+            this.buttonCheck.Text = "Sprawdź";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Location = new System.Drawing.Point(30, 223);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(291, 163);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Pytanie 3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(0, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(260, 50);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Jak nazywa się 3 planeta\r\n naszego układu słonecznego\r\n";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton Button4;
+        private System.Windows.Forms.RadioButton Button3;
+        private System.Windows.Forms.RadioButton Button2;
+        private System.Windows.Forms.RadioButton Button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton Buttonłyba;
+        private System.Windows.Forms.RadioButton Buttonkot;
+        private System.Windows.Forms.RadioButton Buttonpapuga;
+        private System.Windows.Forms.RadioButton Buttonpies;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TestApp/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class MainForm : Form
+    {
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private int testResults()
+        {
+            int wynik = 0;
+
+
+            if (Button4.Checked)
+            {
+                wynik++;
+            }
+            if (Buttonłyba.Checked)
+            {
+                wynik++;
+            }
+            if (checkBox1.Checked && checkBox2.Checked && checkBox3.Checked)
+            {
+                wynik++;
+                if (checkBox1.Checked && checkBox2.Checked && checkBox3.Checked && checkBox4.Checked)
+                {
+                    wynik--;
+                }
+
+            }
+            if ((textBox1.Text.ToLower().Trim() == "ziemia") || (textBox1.Text.ToLower().Trim() == "earth"))
+            {
+                wynik++;
+            }
+
+
+
+            return wynik;
+        }
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+
+
+            MessageBox.Show("Ilosc prawidłowych: " + testResults());
+        }
+
+       
+    }
+}
