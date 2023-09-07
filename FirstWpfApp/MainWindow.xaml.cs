7ab577186655cb7d095d55ee8485dc8a140f308f
Test <test@test.pl>
2023-09-07 12:57:57 +0200

    ezz

--- FirstWpfApp/MainWindow.xaml.cs
+++ FirstWpfApp/MainWindow.xaml.cs
@@ -24,5 +24,26 @@ namespace FirstWpfApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Komunikat po klikioniecuiiu");
+            
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Podałeś wartość:" + valueTextBox.Text);
+        }
     }
 }
