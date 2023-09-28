7f99c35f22864746f285bdaf8ffc7369c915fe19
Test <test@test.pl>
2023-09-28 12:58:44 +0200

    eko

---
+++ ComboboxWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComboboxWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public ObservableCollection<string> ItemsList { get; set; }
+        public string NewPosition { get; set; }
+
+        #region informowanie bindowania
+        public ObservableCollection<OwnColor> OwnColorsCollection { get; set; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        public MainWindow()
+        {
+            OwnColorsCollection = new ObservableCollection<OwnColor>();
+            OwnColorsCollection.Add(new OwnColor()
+            {
+                NameOfColor_Pol = "Zielony",
+                NameOfColor_Eng = "Green"
+            });
+            OwnColorsCollection = new ObservableCollection<OwnColor>();
+            OwnColorsCollection.Add(new OwnColor()
+            {
+                NameOfColor_Pol = "Czerwony",
+                NameOfColor_Eng = "Red"
+            });
+            OwnColorsCollection = new ObservableCollection<OwnColor>();
+            OwnColorsCollection.Add(new OwnColor()
+            {
+                NameOfColor_Pol = "Złoty",
+                NameOfColor_Eng = "Gold"
+            });
+
+            InitializeComponent();
+            ItemsList = new ObservableCollection<string>();
+            OnPropertyChanged(nameof(ItemsList));
+
+            ItemsList.Add("Pozycja bindowania 1");
+            ItemsList.Add("Pozycja bindowania 2");
+            ItemsList.Add("Pozycja bindowania 3");
+        }
+
+        private void ButtonNewComboboxPosition_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsList.Add(NewPosition);
+            OnPropertyChanged(nameof(ItemsList));
+        }
+    }
+}
