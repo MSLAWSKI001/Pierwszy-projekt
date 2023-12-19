c7744cfb76d417844b35334101bf48db82d23af0
Test <test@test.pl>
2023-12-19 13:09:02 +0100

    hj

---
+++ PersionalQuestionnaireWpfApp/Models/PersonalDataModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersionalQuestionnaireWpfApp.Models
+{
+    struct PersonalDataModel
+    {
+        public string name;
+        public bool isPizzaWithPineapple;
+        public bool isMale;
+        public bool isFemale;
+        public List<string> listOfDish;
+        public string selectedDish;
+        public PersonalDataModel()
+        {
+            name = "";
+            isPizzaWithPineapple = false;
+            isMale = false;
+            isFemale = false;
+            listOfDish = new List<string>()
+            {
+                "rosół","schabowy","mielone","kebab"
+            };
+            selectedDish = listOfDish.First();
+        }
+    }
+}
---
+++ PersionalQuestionnaireWpfApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,111 @@
+﻿using PersionalQuestionnaireWpfApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UtilsWpf;
+
+namespace PersionalQuestionnaireWpfApp.ViewModels
+{
+    public class MainViewModel : ObserverVM
+    {
+        private PersonalDataModel personalDataModel = new PersonalDataModel();
+
+        public string Name
+        {
+            get { return personalDataModel.name; }
+            set
+            {
+                personalDataModel.name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public bool PizzaWithPineapple
+        {
+            get { return personalDataModel.isPizzaWithPineapple; }
+            set
+            {
+                personalDataModel.isPizzaWithPineapple = value;
+                OnPropertyChanged(nameof(PizzaWithPineapple));
+            }
+        }
+
+
+        private string questionnaireResult;
+        public string QuestionnaireResult
+        {
+            get { return questionnaireResult; }
+            set
+            {
+                questionnaireResult = value;
+                OnPropertyChanged(nameof(QuestionnaireResult));
+            }
+        }
+    
+        public bool IsMale
+        
+        {
+            get { return personalDataModel.isMale; }
+            set
+            {
+                personalDataModel.isMale = value;
+                OnPropertyChanged(nameof(IsMale));
+            }
+        }
+
+        public bool IsFemale 
+        
+        {
+            get { return personalDataModel.isFemale; }
+            set
+            {
+                personalDataModel.isMale = value;
+                OnPropertyChanged(nameof(IsFemale));
+            }
+        }
+        public List<string> ListOfDish
+
+        {
+            get { return personalDataModel.listOfDish; }
+            set
+            {
+                personalDataModel.listOfDish = value;
+                OnPropertyChanged(nameof(ListOfDish));
+            }
+        }
+        public string SelectedDish
+
+        {
+            get { return personalDataModel.selectedDish; }
+            set
+            {
+                personalDataModel.selectedDish = value;
+                OnPropertyChanged(nameof(SelectedDish));
+            }
+        }
+        private ICommand downloadDataCommand;
+        public ICommand DownloadDataComand
+        {
+            get
+            {
+                if (downloadDataCommand == null)
+                    downloadDataCommand = new RelayCommand<object>(
+                        o =>
+                        {
+                            string result = "";
+                            result += "Imię: " + Name + "\n";
+                            result += "Pizza z ananasem: " + (PizzaWithPineapple ? "Tak" : "Nie") + "\n";
+                            result += "Płeć: " + (IsMale ? "mężczyzna" : "kobieta") + "\n";
+                            result += "Ulubione danie: " + SelectedDish + "\n";
+                            QuestionnaireResult = result;
+                        }
+                        );
+                return downloadDataCommand;
+            }
+        }
+    }
+
+}
