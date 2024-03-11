d484069f01ed9a7e64ac91b70802fe643f49aa8b
Test <test@test.pl>
2024-03-11 14:28:52 +0100

    kk

---
+++ ListaApp/ListaApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListaApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RejestracjaPage());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
---
+++ ListaApp/ListaApp/RejestracjaPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListaApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RejestracjaPage : ContentPage
+    {
+        public RejestracjaPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string password = password1.Text;
+            string password2 = re_password.Text;
+            if (password == password2)
+            {
+
+            }
+            
+            
+                
+        
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
