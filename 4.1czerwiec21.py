92007ca3460b108b010a9d8aa0a47aba27499b30
MSLAWSKI001 <113010458+MSLAWSKI001@users.noreply.github.com>
2024-10-21 13:56:27 +0200

    Delete 4.1czerwiec21.py

--- 4.1czerwiec21.py
+++
@@ -1,3 +0,0 @@
-f = open("przyklad.txt","r")
-chars = f.read()
-print(chars.count("0")+chars.count("1")+chars.count("2")+chars.count("3")+chars.count("4")+chars.count("5")+chars.count("6")+chars.count("7")+chars.count("8")+chars.count("9"))
