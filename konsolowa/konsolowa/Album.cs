281fe49b00017b0d689479769bb5290002e4e051
MSLAWSKI001 <113010458+MSLAWSKI001@users.noreply.github.com>
2024-11-06 12:19:58 +0100

    Add files via upload

---
+++ konsolowa/konsolowa/Album.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsolowa
+{
+    internal class Album
+    {
+        public string artist { get; set; }
+
+        public string album { get; set; }
+
+        public string songsNumber { get; set; }
+
+        public uint year { get; set; }
+        public int downloadNumber { get; set; }
+
+        public string newLine { get; set; }
+    }
+}
